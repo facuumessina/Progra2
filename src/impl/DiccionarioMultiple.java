d418404c992b6ee6740217ab8a0f1fdbd4b9c750
Facundo Messina <facuamessina@gmail.com>
2023-04-18 19:49:10 -0300

    Clase 6

--- src/impl/DiccionarioMultiple.java
+++ src/impl/DiccionarioMultiple.java
@@ -5,6 +5,23 @@ import tda.DiccionarioMultipleTDA;
 
 public class DiccionarioMultiple implements DiccionarioMultipleTDA {
 
+	class Elemento{
+		int clave;
+		int [] valores;
+		int cantValores;
+	}
+	
+	Elemento[] elementos;
+	int cantClaves;
+	
+	private int clave2Indice(int cl) {
+		int pos = - 1;
+		while (pos >= 0 && elementos[pos].clave!=cl) {
+			pos--;
+		}
+		return pos ;
+	}
+	
 	@Override
 	public void inicializarDiccionario() {
 		// TODO Auto-generated method stub
@@ -13,8 +30,32 @@ public class DiccionarioMultiple implements DiccionarioMultipleTDA {
 
 	@Override
 	public void agregar(int clave, int valor) {
-		// TODO Auto-generated method stub
+		int posC = clave2Indice(clave);
+		if (posC == 1) { // la clave no existe
+			posC = cantClaves;
+			elementos[posC] = new Elemento();
+			elementos[posC].clave = clave;
+			elementos[posC].cantValores = 0;
+			elementos[posC].valores = new int[100];
+			cantClaves++;
+		}
+		
+		Elemento e = elementos[posC];
+		int posV = valor2Indice(e,valor);
+		if (posV == -1) { // el valor no existe
+			e.valores[e.cantValores] = valor;
+			e.cantValores++;
+		}
+		
 
+	}
+	
+	private int valor2Indice(Elemento e, int valor) {
+		int i = e.cantValores -1;
+		while (i >= 0 && e.valores[i] != valor) {
+			i--;
+		}
+		return i;
 	}
 
 	@Override
@@ -25,20 +66,40 @@ public class DiccionarioMultiple implements DiccionarioMultipleTDA {
 
 	@Override
 	public void eliminarValor(int clave, int valor) {
-		// TODO Auto-generated method stub
+		int posC = clave2Indice(clave);
+		if (posC != -1) {// la clave existe
+			Elemento e = elementos[posC];
+			int posV = valor2Indice(e,valor);
+			if (posV != -1) { // el valor existe
+				e.valores[posV] = e.valores[e.cantValores - 1];
+				e.cantValores--;
+			}
+			if(e.cantValores == 0) { // verificación final
+				eliminar(clave); // se elimina la clave
+			}
+		}
 
 	}
 
 	@Override
 	public ConjuntoTDA recuperar(int clave) {
-		// TODO Auto-generated method stub
-		return null;
+		ConjuntoTDA c = new Conjunto();
+		c.inicializarConjunto();
+		int pos = clave2Indice(clave);
+		if(pos != -1) { // la clave existe
+			Elemento e = elementos[pos];
+			for (int i = 0; i < e.cantValores; i++) {
+				c.agregar(e.valores[i]);
+			}
+		}
+		return c;
 	}
 
 	@Override
 	public ConjuntoTDA claves() {
-		// TODO Auto-generated method stub
-		return null;
+		ConjuntoTDA c = new Conjunto();
+		c.inicializarConjunto();
+		return c;
 	}
 
 }
--- src/impl/DiccionarioSimple.java
+++ src/impl/DiccionarioSimple.java
@@ -1,37 +1,71 @@
 package impl;
 
+import impl.DiccionarioSimple.Elemento;
 import tda.ConjuntoTDA;
 import tda.DiccionarioSimpleTDA;
 
 public class DiccionarioSimple implements DiccionarioSimpleTDA {
 
+	class Elemento{
+		int clave,valor;
+	}
+	
+	private Elemento[] arr;
+	private int indice;
+	
 	@Override
 	public void inicializarDiccionario() {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	private int clave2Indice(int cl) {
+		int pos = indice - 1;
+		while (pos >= 0 && arr[pos].clave!=cl) {
+			pos--;
+		}
+		return pos ;
+	}
+	
 	@Override
-	public void agregar(int clave, int x) {
-		// TODO Auto-generated method stub
+	public void agregar(int key, int v) {
+		int pos = clave2Indice(key);
+		if(pos == -1) { // la clave no existe
+			arr[indice] = new Elemento();
+			arr[indice].clave = key;
+			arr[indice].valor = v;
+			indice++;
+		}else { //la clave ya existía
+			arr[pos].valor = v;
+		}
 
 	}
 
 	@Override
 	public void eliminar(int clave) {
-		// TODO Auto-generated method stub
+		int pos = clave2Indice(clave);
+		if (pos != -1) {// la clave existe
+			arr[pos] = arr[indice-1]; //ToDo (tener cuidado ya que estamos igualando referencias)
+			// arr[indice-1] = null;
+			/*arr[pos].clave = arr[indice-1].clave;
+			 * arr[pos].valor = arr[indice-1].valor;
+			 */
+			arr[indice-1] = null; //posible
+			indice--;
+		}
 
 	}
 
 	@Override
-	public int recuperar(int clave) {
-		// TODO Auto-generated method stub
+	public int recuperar(int clave) { //muy sencillo
 		return 0;
+
 	}
 
 	@Override
 	public ConjuntoTDA claves() {
-		// TODO Auto-generated method stub
+		// declarar un conjunto, inicializarlo, una vez inicializado, recorrer el arreglo con un for desde 0 a -1 o viceversa
+		// agarra clave y la mete al conjunto, devuelve el conjunto completado con claves
 		return null;
 	}
 
